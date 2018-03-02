4c9da69eb69b83c67edbb675bfae82beec24de41
mattizatt140 <mattizatt140@gmail.com>
2018-03-02 14:09:49 -0600

    New

---
+++ Surface area of a tank/CylinderA.h
@@ -0,0 +1,20 @@
+#pragma once
+class CylinderA {
+public:
+	const double PI{ 3.1415926535 };
+
+	double OuterArea(double r, double h) {
+		double area = 2 * PI * r * h;
+		return area;
+	}
+
+	double TopArea(double r, double h) {
+		double area = PI * (r * r);
+		return area;
+	}
+
+	double TotalArea(double out, double top) {// accepts results of previous two functions
+		double area = out + (2 * top);
+		return area;
+	}
+};
